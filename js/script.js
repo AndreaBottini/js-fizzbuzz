8a7d2e3e24ad3a4c91322097eb3261a24ee23b81
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-09 16:51:05 +0100

    Update e completamento esercizio

--- js/script.js
+++ js/script.js
@@ -9,14 +9,25 @@
 // che di 5 stampi FizzBuzz.
 
 for (var i = 1; i <= 100; i++) {
-  console.log(i);
 
-  if (i % 3 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
+    document.getElementById('multipli').innerHTML += "<li>" + 'Fizzbuzz' + "</li>";
+  }
+
+  else if (i % 3 == 0) {
     console.log('Fizz');
+    document.getElementById('multipli').innerHTML += "<li>" + 'Fizz' + "</li>";
   }
 
   else if (i % 5 == 0) {
     console.log('Buzz');
+    document.getElementById('multipli').innerHTML += "<li>" + 'Buzz' + "</li>";
+  }
+
+  else {
+    console.log(i);
+    document.getElementById('multipli').innerHTML += "<li>" + i + "</li>";
   }
 
 }
